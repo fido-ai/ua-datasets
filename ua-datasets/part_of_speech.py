3b5a4b1fbefa27df1bf6ad8d998b74dc8efbe779
Craq <Craq@users.noreply.github.com>
2021-04-26 17:12:12 +0300

    (refactor) Dataset class
    Moved to Pathlib; 
    Added typing to all methods;

--- ua-datasets/part_of_speech.py
+++ ua-datasets/part_of_speech.py
@@ -1,14 +1,15 @@
-import os
 from urllib import request
-
+from pathlib import Path
+from typing import Any, Tuple, List
 
 class MovaInstitutePOSDataset:
 
     data_file = 'https://lab.mova.institute/files/robochyi_tb.conllu.txt'
 
-    def __init__(self, root: str, train: bool = True, download: bool = True):
+    def __init__(self, root: Path, train: bool = True, download: bool = True) -> None:
         self.root = root
-        self.file_name = 'dataset.txt'
+        self.file_name = 'mova_institue_pos_dataset.txt'
+        self.dataset_path = self.root / self.file_name
         if download:
             self.download()
 
@@ -17,49 +18,49 @@ class MovaInstitutePOSDataset:
                                ' You can use download=True to download it')
 
         self.samples, self.targets = self.load_data(
-            os.path.join(self.root, self.file_name))
+            self.dataset_path)
 
     @property
-    def labels(self):
+    def labels(self) -> List[Any]:
         return self.targets
 
     @property
-    def data(self):
+    def data(self) -> List[Any]:
         return self.samples
 
-    def load_data(self, file_path):
+    def load_data(self, file_path) -> Tuple[List[Any], List[Any]]:
         samples, targets = list(), list()
         curr_sample, curr_target = list(), list()
         with open(file_path, 'r', encoding='utf8') as file:
-            for idx, line in enumerate(file):
+            for line in file:
                 if line[0].isdigit():
                     line = line.split('\t')
                     curr_sample.append(line[1])
                     curr_target.append(line[3])
-                if line[0] == '\n':
+                elif line[0] == '\n':
                     samples.append(curr_sample)
                     targets.append(curr_target)
                     curr_sample, curr_target = list(), list()
         return samples, targets
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx) -> Tuple[Any, Any]:
         sample = self.samples[idx]
         target = self.targets[idx]
         return sample, target
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.data)
 
     def _check_exists(self) -> bool:
-        return os.path.exists(os.path.join(self.root, self.file_name))
+        return self.dataset_path.exists()
 
-    def download(self):
+    def download(self) -> None:
         if self._check_exists():
             return
 
-        os.makedirs(self.root, exist_ok=True)
+        self.root.mkdir(exist_ok=True)
 
         text = request.urlopen(self.data_file).read().decode('utf8')
-        with open(os.path.join(self.root, self.file_name), 'w', encoding='utf8') as f:
+        with open(self.dataset_path, 'w', encoding='utf8') as f:
             for line in text.split('\\n'):
                 f.write(line + '\n')
