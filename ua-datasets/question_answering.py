24c91fb48e85749d4f4d673599b7e0df4739d181
Iron Tony <lifegamecom1@gmail.com>
2021-04-28 22:05:54 +0300

    Added context to batch, refactoring

--- ua-datasets/question_answering.py
+++ ua-datasets/question_answering.py
@@ -22,15 +22,19 @@ class UaSquadDataset:
             raise RuntimeError('Dataset not found. ' +
                                'You can use download=True to download it')
 
-        self._samples, self._targets = self.load_data(self.dataset_path)
+        self._questions, self._contexts, self._answers = self.load_data(self.dataset_path)
 
     @property
-    def labels(self) -> List[Any]:
-        return self._targets
+    def answers(self) -> List[Any]:
+        return self._answers
 
     @property
-    def data(self) -> List[Any]:
-        return self._samples
+    def contexts(self) -> List[Any]:
+        return self._contexts
+
+    @property
+    def questions(self) -> List[Any]:
+        return self._questions
 
     def download(self) -> None:
         if self._check_exists():
@@ -42,34 +46,38 @@ class UaSquadDataset:
         with open(self.dataset_path, 'w', encoding='utf8') as f:
             f.write(text)
 
-    def load_data(self, file_path) -> Tuple[List[Any], List[Any]]:
-        samples, targets = list(), list()
+    @staticmethod
+    def load_data(file_path) -> Tuple[List[Any], List[Any], List[Any]]:
+        questions, contexts, answers = list(), list(), list()
 
         with open(file_path, 'r', encoding='utf8') as file:
             json_obj = json.load(file)
 
         for topic in json_obj["data"]:
             for paragraph in topic["paragraphs"]:
+                context = paragraph["context"]
                 for qa in paragraph["qas"]:
                     question = qa["question"]
-                    answers = qa["answers"]
-                    if answers:
-                        answer = answers[0]["text"]
+                    question_answers = qa["answers"]
+                    if question_answers:
+                        answer = question_answers[0]["text"]
                     else:
                         answer = None
 
-                    samples.append(question)
-                    targets.append(answer)
+                    questions.append(question)
+                    contexts.append(context)
+                    answers.append(answer)
 
-        return samples, targets
+        return questions, contexts, answers
 
-    def __getitem__(self, idx) -> Tuple[Any, Any]:
-        sample = self._samples[idx]
-        target = self._targets[idx]
-        return sample, target
+    def __getitem__(self, idx) -> Tuple[Any, Any, Any]:
+        question = self._questions[idx]
+        context = self._contexts[idx]
+        answer = self._answers[idx]
+        return question, context, answer
 
     def __len__(self) -> int:
-        return len(self._samples)
+        return len(self._questions)
 
     def _check_exists(self) -> bool:
         return self.dataset_path.exists()
