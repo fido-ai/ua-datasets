b316176938e0f193182dea13dbe136b03f801886
Iron Tony <lifegamecom1@gmail.com>
2021-09-05 17:42:30 +0300

    pep fixes

--- ua-datasets/classification_dataset.py
+++ ua-datasets/classification_dataset.py
@@ -1,6 +1,8 @@
 import json
 from pathlib import Path
-from typing import Any, Tuple, List
+from typing import Tuple, List
+from urllib import request
+
 
 class ClassificationDataset:
     def __init__(self, root: str, download: bool = False, return_tags: bool = False) -> None:
@@ -8,20 +10,26 @@ class ClassificationDataset:
         self.return_tags = return_tags
         self.file_name = 'cl_data_Anton.json'
         self.dataset_path = self.root / self.file_name
-        
+
         if download:
             self.download()
-        
-        if not self._check_exists():
-            raise RuntimeError('Dataset not found. ' +
-                                'You can use download=True to download it')
 
-        self._samples_titles, self._samples_texts, self._targets, self._tags = self.load(self.dataset_path)
-        
-    def _check_exists(self) -> bool:
-        return self.dataset_path.exists()
-    
-    def load(self, file_path) -> Tuple[List[str], List[str], List[str]]:
+        if not self._check_exists():
+            raise RuntimeError('Dataset not found. '
+                               'You can use download=True to download it')
+
+        self._samples_titles, self._samples_texts, self._targets, self._tags = self.load()
+
+    @property
+    def labels(self) -> List[str]:
+        return self._targets
+
+    @property
+    def data(self) -> List[str]:
+        return self._samples
+
+    @staticmethod
+    def load(file_path) -> Tuple[List[str], List[str], List[str]]:
         samples_title, samples_text, targets, tags = list(), list(), list(), list()
 
         with open(file_path, 'r', encoding='utf8') as file:
@@ -35,26 +43,17 @@ class ClassificationDataset:
 
         return samples_title, samples_text, targets, tags
 
-
     def download(self) -> None:
         if self._check_exists():
-            return 
-        
+            return
+
         self.root.mkdir(exist_ok=True)
-        
+
         text = request.urlopen(self.data_file).read().decode('utf8')
         with open(self.dataset_path, 'w', encoding='utf8') as f:
             for line in text.split('\\n'):
                 f.write(line + '\n')
-                
-    @property
-    def labels(self) -> List[str]:
-        return self._targets
-    
-    @property
-    def data(self) -> List[str]:
-        return self._samples
-    
+
     def __getitem__(self, idx) -> Tuple[str, str]:
         sample_title = self._samples_titles[idx]
         sample_text = self._samples_texts[idx]
@@ -63,8 +62,12 @@ class ClassificationDataset:
         if self.return_tags:
             tags = self._tags[idx]
             return sample_title, sample_text, target, tags
-        
+
         return sample_title, sample_text, target
+
+    def _check_exists(self) -> bool:
+        return self.dataset_path.exists()
+
 
 if __name__ == '__main__':
     dataset = ClassificationDataset('/Users/bogdanivanyuk/Desktop/BERT_train_data/classification_data', return_tags=True)
