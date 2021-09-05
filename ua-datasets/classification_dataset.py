3c4c97661ce4deeae44bf5a197c0287d5a8e6b44
Iron Tony <lifegamecom1@gmail.com>
2021-09-05 17:59:48 +0300

    style and code fixes

--- ua-datasets/classification_dataset.py
+++ ua-datasets/classification_dataset.py
@@ -5,6 +5,8 @@ from urllib import request
 
 
 class ClassificationDataset:
+    _data_link = ''  # TODO real link
+
     def __init__(self, root: str, download: bool = False, return_tags: bool = False) -> None:
         self.root = Path(root)
         self.return_tags = return_tags
@@ -18,7 +20,7 @@ class ClassificationDataset:
             raise RuntimeError('Dataset not found. '
                                'You can use download=True to download it')
 
-        self._samples_titles, self._samples_texts, self._targets, self._tags = self.load()
+        self._samples_titles, self._samples_texts, self._targets, self._tags = self.parse(str(self.dataset_path))
 
     @property
     def labels(self) -> List[str]:
@@ -26,22 +28,9 @@ class ClassificationDataset:
 
     @property
     def data(self) -> List[str]:
-        return self._samples
+        return self._samples  # TODO no such field
 
-    @staticmethod
-    def load(file_path) -> Tuple[List[str], List[str], List[str]]:
-        samples_title, samples_text, targets, tags = list(), list(), list(), list()
-
-        with open(file_path, 'r', encoding='utf8') as file:
-            json_obj = json.load(file)
-
-        for idx in json_obj.keys():
-            samples_title.append(json_obj[idx]['title'].rstrip().lstrip())
-            samples_text.append(json_obj[idx]['text'])
-            targets.append(json_obj[idx]['true_label'])
-            tags.append(json_obj[idx]['tags'].split('|')[1:-1])
-
-        return samples_title, samples_text, targets, tags
+    # TODO other properties?
 
     def download(self) -> None:
         if self._check_exists():
@@ -49,12 +38,27 @@ class ClassificationDataset:
 
         self.root.mkdir(exist_ok=True)
 
-        text = request.urlopen(self.data_file).read().decode('utf8')
+        text = request.urlopen(ClassificationDataset._data_link).read().decode('utf8')
         with open(self.dataset_path, 'w', encoding='utf8') as f:
-            for line in text.split('\\n'):
+            for line in text.split('\\n'):  # TODO are we sure 'f.write(text)' is not enough?
                 f.write(line + '\n')
 
-    def __getitem__(self, idx) -> Tuple[str, str]:
+    @staticmethod
+    def parse(file_path: str) -> Tuple[List[str], List[str], List[str], List[str]]:
+        samples_title, samples_text, targets, tags = list(), list(), list(), list()
+
+        with open(file_path, 'r', encoding='utf8') as file:
+            json_obj = json.load(file)
+
+        for idx in json_obj.keys():
+            samples_title.append(json_obj[idx]['title'].strip())
+            samples_text.append(json_obj[idx]['text'])
+            targets.append(json_obj[idx]['true_label'])
+            tags.append(json_obj[idx]['tags'].split('|')[1:-1])
+
+        return samples_title, samples_text, targets, tags
+
+    def __getitem__(self, idx: int):
         sample_title = self._samples_titles[idx]
         sample_text = self._samples_texts[idx]
         target = self._targets[idx]
@@ -64,6 +68,9 @@ class ClassificationDataset:
             return sample_title, sample_text, target, tags
 
         return sample_title, sample_text, target
+
+    def __len__(self) -> int:
+        return len(self._samples_titles)
 
     def _check_exists(self) -> bool:
         return self.dataset_path.exists()
