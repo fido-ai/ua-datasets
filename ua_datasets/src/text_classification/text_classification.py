f93ab8b7fbe34ac259959d8e74b832e05c429915
KyloRen1 <ivanyuk.bogdan1999@yahoo.com>
2021-10-05 20:55:34 +0300

    fixed;

--- ua_datasets/src/text_classification/text_classification.py
+++ ua_datasets/src/text_classification/text_classification.py
@@ -1,46 +1,71 @@
 import csv
 import pathlib
+from urllib import request
 from typing import Tuple, List
 
 
 class NewsClassificationDataset:
-	def __init__(self, split: str = 'train', return_tags: bool = False):
+
+	data_file = 'https://github.com/fido-ai/ua-datasets/releases/download/v0.0.1/'
+
+	def __init__(self, root: str, download: bool = True, split: str = 'train', return_tags: bool = False):
 		""" News Classification Dataset
 
 		Args:
+			root (:obj: `str`): direcoty path
+			download (:obj: `bool`): Whether to download data 
 			split (:obj: `str`): Which split of the data to load (train or test)
 			return_tags (:obj: `bool`, optional): Whether to return text keywords 
 			
 		"""
-		self.path = (pathlib.Path(__file__) / ".." / ".." / "..").resolve()
-		self.return_tags = return_tags
 		self.split = split
+		self.return_tags = return_tags
+		self.root = pathlib.Path(root)
+		self.dataset_path = self.root / (self.split + '.csv')
+
+		if download:
+			self.download()
+
+		if not self._check_exists():
+			raise RuntimeError('Dataset not found.' + 
+							   ' You can use download=True to download it')
+		
 		self._data = self._load_data()
+
+
+	def _check_exists(self) -> bool:
+		return self.dataset_path.exists()
+
+	def download(self) -> None:
+		if self._check_exists():
+			return
+
+		self.root.mkdir(exist_ok=True)
+
+		text = request.urlopen(self.data_file + self.split + '.csv').read().decode('utf8')
+		with open(self.dataset_path, 'w', encoding='utf8') as f:
+			for line in text.split('\\n'):
+				f.write(line + '\n')
 
 	def _load_data(self) -> List[List[str]]:
 		""" Load dataset """
-		if self.split == 'train':
-			data_path = self.path / 'data/text_classification/train.csv'
-		elif self.split == 'test':
-			data_path = self.path / 'data/text_classification/test.csv'
-		else:
-			raise NotImplementedError()
-
-		file = open(data_path)
+		file = open(self.dataset_path)
 		csvreader = csv.reader(file)
-		self.columns = next(csvreader)
+		self._columns = next(csvreader)
 		samples = list()
 		for row in csvreader:
 			samples.append(row)
 		return samples
 
+	@property
 	def column_names(self) -> List[str]:
 		""" Dataset column names """
-		return self.columns
+		return self._columns
 
+	@property
 	def labels(self) -> List[str]:
 		""" Target names """
-		return set([row[self.columns.index('target')] for row in self._data])
+		return set([row[self._columns.index('target')] for row in self._data[:-1]])
 		
 	def __getitem__(self, idx: int) -> Tuple[str]:
 		title, text, tags, target = self._data[idx]
