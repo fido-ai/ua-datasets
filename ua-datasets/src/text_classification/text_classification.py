a72bdbe5c48fa724169bd825be551412963a82dd
KyloRen1 <ivanyuk.bogdan1999@yahoo.com>
2021-10-05 09:08:11 +0300

    minr fixes

--- ua-datasets/src/text_classification/text_classification.py
+++ ua-datasets/src/text_classification/text_classification.py
@@ -3,16 +3,16 @@ import pathlib
 from typing import Any, Tuple, List
 
 class NewsClassification:
-	def __init__(self, split: str = 'train', return_keywords: bool = False):
+	def __init__(self, split: str = 'train', return_tags: bool = False):
 		""" News classification dataset
 
 		Args:
 			split (:obj: `str`): Which split of the data to load (train or test)
-			return_keywords (:obj: `bool`, optional): Whether to return text keywords 
+			return_tags (:obj: `bool`, optional): Whether to return text keywords 
 			
 		"""
 		self.path = (pathlib.Path(__file__) / ".." / ".." / "..").resolve()
-		self.return_keywords = return_keywords
+		self.return_tags = return_tags
 		self.split = split
 		self._data = self._load_data()
 		
@@ -24,7 +24,7 @@ class NewsClassification:
 			data_path = self.path / 'data/text_classification/test.csv'
 		else:
 			raise NotImplementedError()
-			
+
 		file = open(data_path)
 		csvreader = csv.reader(file)
 		self.columns = next(csvreader)
@@ -39,23 +39,28 @@ class NewsClassification:
 
 	def labels(self) -> List[str]:
 		""" Target names """
-		return set([row[-1] for row in self._data])
-		#return set([row[-1] for row in self._data])
+		return set([row[self.columns.index('target')] for row in self._data])
 		
 	def __getitem__(self, idx) -> Tuple[str]:
 		title, text, tags, target = self._data[idx]
-		if self.return_keywords:
+		if self.return_tags:
+			tags = self._preprocess_tags(tags)
 			return title, text, tags, target
 		else:
 			return title, text, target
 	
+	def _preprocess_tags(self, tags) -> List[str]:
+		""" Text tags preprocessing """
+		return [el for el in tags.split("|") if el != '']
+
 	def __len__(self):
 		""" Number of rows in the dataset """
 		return len(self._data)
 
 if __name__ == '__main__':
-	data = NewsClassification() 
+	data = NewsClassification(return_tags=True) 
 	print(len(data))
+	print(data[0])
 	print(data.labels())
 
 	data = NewsClassification(split='test')
